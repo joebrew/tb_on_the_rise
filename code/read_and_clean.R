0bf2378ca1e4a29500bfdb6af4a3d3b3aaf72d2d
Joe Brew <joebrew@gmail.com>
2016-04-27 15:14:55 +0200

    updating minor things

--- code/read_and_clean.R
+++ code/read_and_clean.R
@@ -140,17 +140,194 @@ tb <-
   mutate(age = gsub(' ', '', age)) %>%
   mutate(years_old = 
            suppressWarnings(
-           ifelse(grepl('D', age),
-                  as.numeric(gsub('D', '', as.character(age))) / 365.25,
-                  ifelse(grepl('M', age),
-                         as.numeric(gsub('M', '', as.character(age))) / 12,
-                         ifelse(age == '999', NA,
-                                as.numeric(as.character(age))))))) %>%
+             ifelse(grepl('D', age),
+                    as.numeric(gsub('D', '', as.character(age))) / 365.25,
+                    ifelse(grepl('M', age),
+                           as.numeric(gsub('M', '', as.character(age))) / 12,
+                           ifelse(age == '999', NA,
+                                  as.numeric(as.character(age))))))) %>%
   # round years
   mutate(years_old_round = 
            round(years_old))
 
+##### GET POPULATION BY YEAR
+# Manually code based on the two html files herein:
+# - Pop_por_distrito_Map_province_2007.htm
+# - Pop_por_distrito_Map_province_1997.htm
+population <- 
+  data.frame(age_group = rep(c('00',
+                               '01-04',
+                               '05-09',
+                               '10-14',
+                               '15-19',
+                               '20-24',
+                               '25-29',
+                               '30-34',
+                               '35-39',
+                               '40-44',
+                               '45-49',
+                               '50-54',
+                               '55-59',
+                               '60-64',
+                               '65-69',
+                               '70-74',
+                               '75-79',
+                               '80+'), 
+                             2))
+# Add the two years
+population$year <-
+  rep(c(1997, 2007), each = nrow(population) / 2)
+# Add the two genders
+males <- population; males$sex <- 'male'
+females <- population; females$sex <- 'female'
+# Overwite population with our genders
+population <- rbind(males, females)
+rm(males, females)
+# Populate with the data from the htmls
+population$n <- NA
+population$n[population$year == 2007 & 
+               population$sex == 'male'] <-
+  c(18294,	
+    71362,	
+    81491,	
+    72298,	
+    61643,	
+    56441,	
+    51213,	
+    38603,	
+    29392,	
+    24123,	
+    21259,	
+    15034,	
+    10760,	
+    7713,
+    6588,	
+    4566,	
+    3012,	
+    2220)
+population$n[population$year == 2007 & 
+               population$sex == 'female'] <- 
+  c(18320, 
+    71862, 
+    83061, 
+    73945, 
+    63691, 
+    67678, 
+    58576, 
+    43714, 
+    33632, 
+    26201, 
+    21532, 
+    17468, 
+    13666, 
+    10311, 
+    8959, 
+    6886, 
+    4988, 
+    5207)
+population$n[population$year == 1997 & 
+               population$sex == 'male'] <-
+  c(11324, 
+    47372, 
+    54564, 
+    55013, 
+    47587, 
+    31514, 
+    24292, 
+    21659, 
+    21395, 
+    15487, 
+    12243, 
+    9277, 
+    8920, 
+    7048, 
+    5492, 
+    2644, 
+    2314, 
+    1644)
+
+population$n[population$year == 1997 & 
+               population$sex == 'female'] <-
+  c(11636, 
+    48501, 
+    55412, 
+    55293, 
+    50270, 
+    41213, 
+    30931, 
+    25934, 
+    22479, 
+    17106, 
+    15577, 
+    12679, 
+    11050, 
+    8971, 
+    7741, 
+    4260, 
+    4044, 
+    3293)
+
+##### ESTIMATE POPULATION FOR YEARS WE DON'T HAVE
+denom <- 
+  expand.grid(year = 1986:2015,
+              sex = c('male', 'female'),
+              age_group = unique(sort(population$age_group)))
+denom$n <- NA
+years <- sort(unique(denom$year))
+n_years <- length(years)
+
+# Fill up those that are already known
+for (i in 1:nrow(denom)){
+  this_row <- denom[i,]
+  n <- 
+    population$n[population$year == this_row$year &
+                   population$sex == this_row$sex &
+                   population$age_group == this_row$age_group]
+  if(length(n) == 1){
+    denom$n[i] <- n
+  }
+}
+
+# Interpolate / Extrapolate the missings
+for (sex in c('male', 'female')){
+  for (age_group in unique(sort(population$age_group))){
+    # Calculate slope
+    sub_data <- population[population$sex == sex & 
+                             population$age_group == age_group,]
+    growth <- sub_data$n[sub_data$year == 2007] - 
+      sub_data$n[sub_data$year == 1997]
+    # Divde growth by number of years (year-specific group growth)
+    by_year <- growth / 10
+    # Add the results into denom
+    for (year in unique(denom$year[denom$sex == sex &
+                                   denom$age_group == age_group])){
+      # See how many years since 1997
+      years_since_1997 <- year - 1997
+      # Get the 1997 val
+      val_1997 <- sub_data$n[sub_data$year == 1997]
+      # Get the value for that year
+      val <- val_1997 + (years_since_1997 * by_year)
+      # Stick that value into results
+      denom$n[denom$sex == sex &
+                denom$age_group == age_group &
+                denom$year == year] <- val
+    }
+  }
+}
+
+# Specify whether the data is from census or estimate
+denom <- 
+  denom %>%
+  left_join(population %>%
+              mutate(data_source = 'census'),
+            by = c("year", "sex", "age_group", "n")) %>%
+  mutate(data_source = ifelse(is.na(data_source), 'estimate', data_source))
+
+# Overwrite population, and remove garbage
+population <- denom
+rm(i, age_group, sex, denom, sub_data, this_row)
+
 # Write csv of cleaned data
 # setwd(data_dir)
-# # write_csv(tb, 'cleaned_data.csv')
+# # write_csv(tb 'cleaned_data.csv')
 # setwd(root)
